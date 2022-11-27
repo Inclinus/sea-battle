30a51ab4a12d60437a1f65a111651ede701133e9
Inclinus <inclinus@outlook.fr>
2022-11-27 09:54:46 +0100

    implement aliases

--- SeaBattle.go
+++ SeaBattle.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 )
+
+type IP struct {
+	ip   string
+	port uint16
+}
 
 func SplitIpAndPort(str string) (string, uint16) {
 	split := strings.Split(str, ":")
@@ -19,6 +25,61 @@ func SplitIpAndPort(str string) (string, uint16) {
 	return ip, ui
 
 }
+
+func addAlias(aliases *map[string]IP, ip string, username string) {
+	realIp, port := SplitIpAndPort(ip)
+	ipStruct := IP{
+		ip:   realIp,
+		port: port,
+	}
+	(*aliases)[username] = ipStruct
+}
+
+func displayAliases(aliases *map[string]IP) {
+	for key, value := range *aliases {
+		fmt.Printf("%s (%s:%d)\n", key, value.ip, value.port)
+	}
+}
+
+func displayAlias(aliases *map[string]IP, username string) {
+	for key, value := range *aliases {
+		if key == username {
+			fmt.Printf("%s (%s:%d)\n", key, value.ip, value.port)
+		}
+	}
+}
+
+func removeAlias(aliases *map[string]IP, username string) {
+	for key, _ := range *aliases {
+		if key == username {
+			delete(*aliases, username)
+			fmt.Println(username + " has been deleted.")
+		}
+	}
+}
+
+func getIpOff(username string, aliases *map[string]IP) (string, uint16) {
+	for key, value := range *aliases {
+		if key == username {
+			return value.ip, value.port
+		}
+	}
+	return "", 0
+}
+
+/*
+func testAliases(aliases *map[string]IP) {
+	addAlias(aliases, "192.168.0.1:55542", "Noam")
+	i, p := getIpOff("Noam", aliases)
+	fmt.Printf("%s:%d\n", i, p)
+	displayAliases(aliases)
+	displayAlias(aliases, "Noam")
+	removeAlias(aliases, "Noam")
+}
+*/
+
 func main() {
+	//aliases := make(map[string]IP)
+	//testAliases(&aliases)
 
 }

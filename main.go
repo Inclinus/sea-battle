fc28cba3e928439029d4bea7130b82b8066287d2
Kyato <thibaut.lulinski@hotmail.fr>
2022-12-11 11:56:25 +0100

    function GetPos v2

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 type Position struct {
@@ -123,7 +124,18 @@ func getPos(inputPos string) Position {
 	return mapOfCord[inputPos]
 }
 
+func getPos2(inputPos string) Position {
+	var pos Position
+	YtoInt, _ := strconv.Atoi(inputPos[1:2])
+	pos.Y = uint8(YtoInt)
+
+	mapOfCord := map[string]byte{"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8, "I": 9, "J": 10}
+	pos.X = mapOfCord[inputPos[:1]]
+
+	return pos
+}
+
 func main() {
-	result := getPos("J6")
+	result := getPos2("J6")
 	fmt.Println(result)
 }

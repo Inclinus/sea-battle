8586bf6f1ad5bc0dd393f0c99bef2e2d07bc342d
csalhabb <charbel.salhab.2003@gmail.com>
2022-12-17 15:42:07 +0100

    updated menu

--- Sea-Battle.go
+++ Sea-Battle.go
@@ -2,23 +2,92 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
-func DisplayMenu() {
+func displayChoices() {
+
+	fmt.Println("Menu :\n" +
+		"1 -  Voir l'état du board \n" +
+		"2 -  Attaquer \n" +
+		"3 -  Gérer les alias \n" +
+		"4 -  Règles du jeu \n" +
+		"5 -  Statistiques \n" +
+		"6 -  Crédits \n" +
+		"7 -  Quitter la session\n\n" +
+		"Quel est votre choix ? ")
+
+}
+
+func manageAliases() {
+	fmt.Println("------------------------------")
+	var ch int
+
+	for ch != 5 {
+
+		fmt.Println("Sous-Menu pour la Gestion des Alias :\n" +
+			"1 - Afficher les Alias\n" +
+			"2 - Afficher l’ip du joueur\n" +
+			"3 - Ajouter un Alias\n" +
+			"4 - Retirer un Alias\n" +
+			"5 - Quitter le Sous-Menu et retourner au Menu Principal\n" +
+			"Quel est votre choix ?\n")
+
+		fmt.Scanf("%d\n", &ch)
+
+		switch ch {
+
+		case 1:
+			//display all the aliases
+		case 2:
+			//display the ip of the player
+		case 3:
+			//Add an alias
+		case 4:
+			//remove an alias
+		case 5:
+			fmt.Println("Retour au Menu Principal!")
+			fmt.Println("------------------------------")
+		default:
+			fmt.Println("Votre choix doit etre entre 1 et 5 !")
+
+		}
+	}
+}
+
+func displayCredits() {
+
+	fmt.Println("Nous souhaitons adresser nos remerciements les plus sincères à Monsieur Karraz qui nous a apporté son aide pour mener à  bien ce projet: " +
+		"\"Jeu de Bataille Navale\"\n" +
+		"Les Contributeurs au cours de ce projet : " +
+		"- Anto BENEDETTI @opixelum\n" +
+		"- Thibaut LULINSKI @KyatoNS\n" +
+		"- Noam DE MASURE @Inclinus\n" +
+		"- Charbel SALHAB @csalhabb\n" +
+		"Merci !\n")
+
+}
+
+func displayRules() {
+
+	fmt.Println("La bataille navale est un des jeux de société qui fait amuser petits et grands. La bataille navale est idéale pour passer un moment en famille. " +
+		"\nVoici les règles du jeu : \n\n" +
+		"- La bataille navale se joue sur une grille faisant au moins 10x10.\n" +
+		"- Le joueur doit deviner où se situent les bateaux adverses afin de les couler.\n" +
+		"- Les bateaux devront être placés aléatoirement sur les grilles des joueurs, une case ne peut être occupée que par un morceau de bateau à la fois.\n" +
+		"- Il doit y avoir plusieurs bateaux présents sur le plateau.\n" +
+		"- Par contre, nous ne jouons pas l’un après l’autre ici, mais en temps réel.\n" +
+		"- N’importe quel joueur peut jouer plusieurs fois d’affilée et en continu sans attendre les actions des autres.\n" +
+		"- Ce n’est pas un jeu au tour par tour.\n")
+
+}
+
+func displayMenu() {
+
 	var choice int
 
-	for true {
-		fmt.Println("Menu :\n" +
-			"1 -  Voir l'état du board \n" +
-			"2 -  Attaquer \n" +
-			"3 -  Visualiser les alias \n" +
-			"4 -  Règles du jeu \n" +
-			"5 -  Statistiques \n" +
-			"6 -  Crédits \n" +
-			"7 -  Quitter la session \n\n" +
-			"Quel est votre choix ? \n")
+	for choice != 7 {
 
+		displayChoices()
 		fmt.Scanf("%d\n", &choice)
 
 		switch choice {
@@ -28,37 +97,24 @@ func DisplayMenu() {
 		case 2:
 			//Attack or start the game
 		case 3:
-			//Visualization of the alias
+			manageAliases()
 		case 4:
-			fmt.Println("")
-			fmt.Println("La bataille navale est un des jeux de société qui fait amuser petits et grands. La bataille navale est idéale pour passer un moment en famille. " +
-				"\nVoici les règles du jeu : \n")
-			fmt.Println("Pour jouer à la bataille navale, il vous faut un plateau de jeu où chacun dispose d’une grille numérotée de 1 à 10 horizontalement et annotée de A à J verticalement," +
-				"ainsi que d'une flotte composée de quelques bateaux d'une à cinq cases de long. " +
-				"\nIl ne faut pas que les participants voient la grille des adversaires.\n")
-			fmt.Println("Au début du jeu, chaque joueur place ses bateaux sur sa grille, " +
-				"en s'assurant que deux bateaux ne sont pas adjacents. L'autre représente la zone adverse, " +
-				"où il cherchera à couler les bateaux de son adversaire.\n")
-			fmt.Println("Chaque joueur, à son tour, annonce une case, et son adversaire lui répond si le tir tombe à l'eau ou au contraire s'il touche un bateau.\nDans ce dernier cas, " +
-				"il annonce « touché » s'il reste des cases intactes au bateau ciblé, et « touché-coulé » si non.\n")
+			displayRules()
 		case 5:
 			//Statistics
 		case 6:
-			fmt.Println("Nous souhaitons adresser nos remerciements les plus sincères à Monsieur Karraz qui nous a apporté son aide pour mener à  bien ce projet.\n")
-			fmt.Println("\"Jeu de Bataille Navale\"\n")
-			fmt.Println("Les Contributeurs au cours de ce projet : \n")
-			fmt.Println("- Anto BENEDETTI\n")
-			fmt.Println("- Thibaut LULINSKI\n")
-			fmt.Println("- Noam DE MASURE\n")
-			fmt.Println("- Charbel SALHAB\n")
-			fmt.Println("Merci !\n")
+			displayCredits()
 		case 7:
-			os.Exit(0)
+			fmt.Println("Vous avez quitté le programme ! ")
 		default:
 			fmt.Println("Votre choix doit etre entre 1 et 7 !")
 		}
 	}
+
 }
+
 func main() {
-	DisplayMenu()
+
+	displayMenu()
+
 }

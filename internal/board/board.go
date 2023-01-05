e9ce944918f6e6666ac6275f22d6fba8731a59d4
KyatoNS <thibaut.lulinski@hotmail.fr>
2023-01-05 20:09:42 +0100

    unfix

--- internal/board/board.go
+++ internal/board/board.go
@@ -247,7 +247,7 @@ func RequestBoard(clientIP ip.IP) {
 // This function get a string in parameter (ex: "J6") and return a Position struct
 func GetPositionFromString(inputPos string) utils.Position {
 	var pos utils.Position
-	YtoInt, _ := strconv.Atoi(inputPos[1:3])
+	YtoInt, _ := strconv.Atoi(inputPos[1:2])
 	pos.Y = uint8(YtoInt)
 
 	mapOfCord := map[string]byte{"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8, "I": 9, "J": 10, "a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "j": 10}
